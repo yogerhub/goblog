1364900631f593bbc97b4e0bfc9d037cbc819579
liyao <liyao@joydata.com>
2021-03-20 10:23:09 +0800

    add about page

--- main.go
+++ main.go
@@ -5,12 +5,20 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprint(w,"<h1>hello</h1>")
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>hello</h1>")
+		fmt.Fprint(w, "请求路径："+r.URL.Path)
+	}else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "<h1>about</h1>")
+		fmt.Fprint(w, "请求路径："+r.URL.Path)
+	}else {
+		fmt.Fprint(w, "<h1>not fund</h1>")
+		fmt.Fprint(w, "请求路径："+r.URL.Path)
+	}
 }
 
 func main() {
-	http.HandleFunc("/",handlerFunc)
-	http.ListenAndServe(":3000",nil)
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
 }
-
