ee60dda872314adcf1dfb17f11e1bfa21d08e4a2
liyao <liyao@joydata.com>
2021-03-20 17:58:27 +0800

    use Http ServeMux

--- main.go
+++ main.go
@@ -3,24 +3,43 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type","text/html;charset=utf-8")
+func DefaultHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html;charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>hello</h1>")
 		fmt.Fprint(w, "请求路径："+r.URL.Path)
-	}else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "<h1>about</h1>")
-		fmt.Fprint(w, "请求路径："+r.URL.Path)
-	}else {
+	} else {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1>not fund</h1>")
 		fmt.Fprint(w, "请求路径："+r.URL.Path)
 	}
 }
 
+func AboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html;charset=utf-8")
+	fmt.Fprint(w, "<h1>About</h1>")
+	fmt.Fprint(w, "请求路径："+r.URL.Path)
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", DefaultHandler)
+	router.HandleFunc("/about", AboutHandler)
+	router.HandleFunc("/articles/", func(writer http.ResponseWriter, request *http.Request) {
+		id := strings.SplitN(request.URL.Path, "/", 3)[2]
+		fmt.Fprint(writer, "文章 ID："+id)
+	})
+	router.HandleFunc("/articles", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			fmt.Fprint(w,"GET访问文章列表")
+		case "POST":
+			fmt.Fprint(w,"POST创建新文章")
+		}
+	})
+	http.ListenAndServe(":3000", router)
 }
