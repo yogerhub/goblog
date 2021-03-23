f5f5db2640386555b9f7fc9544f49c5323057323
liyao <liyao@joydata.com>
2021-03-23 09:04:18 +0800

    complete test case

--- main.go
+++ main.go
@@ -438,7 +438,7 @@ func main() {
 	router.HandleFunc("/articles", articlesIndexHandle).Methods("GET").Name("articles.index")
 	router.HandleFunc("/articles/create", articlesCreateHandler).Methods("GET").Name("articles.create")
 	router.HandleFunc("/articles/{id:[0-9]+}/edit", articlesEditHandler).Methods("GET").Name("articles.edit")
-	router.HandleFunc("/articles/{id:[0-9]+}/update", articlesUpdateHandler).Methods("POST").Name("articles.update")
+	router.HandleFunc("/articles/{id:[0-9]+}", articlesUpdateHandler).Methods("POST").Name("articles.update")
 	router.HandleFunc("/articles/{id:[0-9]+}/delete", articlesDeleteHandler).Methods("POST").Name("articles.delete")
 	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
 
--- tests/pages_test.go
+++ tests/pages_test.go
@@ -3,19 +3,48 @@ package tests
 import (
 	"github.com/stretchr/testify/assert"
 	"net/http"
+	"strconv"
 	"testing"
 )
 
-func TestHomePage(t *testing.T) {
+func TestAllPages(t *testing.T) {
 	baseURL := "http://localhost:3000"
-	var (
-		resp *http.Response
-		err  error
-	)
-	// 1. 请求 —— 模拟用户访问浏览器
-	resp, err = http.Get(baseURL + "/")
+	// 1. 申明加初始化测试数据
+	var tests = []struct{
+		method string
+		url string
+		expected int
+	}{
+		{"GET", "/", 200},
+		{"GET", "/about", 200},
+		{"GET", "/notfound", 404},
+		{"GET", "/articles", 200},
+		{"GET", "/articles/create", 200},
+		{"GET", "/articles/3", 200},
+		{"GET", "/articles/3/edit", 200},
+		{"POST", "/articles/3", 200},
+		{"POST", "/articles", 200},
+		{"POST", "/articles/5/delete", 404},
+	}
 
-	// 2. 检测 —— 是否无错误且 200
-	assert.NoError(t, err, "有错误发生，err 不能为空")
-	assert.Equal(t, 200, resp.StatusCode, "应该返回状态码 200")
+	// 2. 遍历所有测试
+	for _,test :=range tests{
+		t.Logf("当前URL：%v \n",test.url)
+		var (
+			resp *http.Response
+			err  error
+		)
+		// 2.1 请求以获取响应
+		switch {
+		case test.method == "POST":
+			data:=make(map[string][]string)
+			resp, err = http.PostForm(baseURL + test.url,data)
+		default:
+			resp, err = http.Get(baseURL + test.url)
+		}
+
+		// 2. 检测 —— 是否无错误且 200
+		assert.NoError(t, err, "请求 "+baseURL+" 报错")
+		assert.Equal(t, test.expected, resp.StatusCode, test.url+"应该返回状态码 "+strconv.Itoa(test.expected))
+	}
 }
