047af201286353109bb81831a29a89af5288b059
liyao <liyao@joydata.com>
2021-03-29 19:23:08 +0800

    register validatiion

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"fmt"
+	"goblog/app/models/user"
+	"goblog/app/requests"
 	"goblog/pkg/view"
 	"net/http"
 )
@@ -14,7 +17,43 @@ func (*AuthController) Register(w http.ResponseWriter, r *http.Request) {
 	view.RenderSimple(w, view.D{}, "auth.register")
 }
 
+type userForm struct {
+	Name            string `valid:"name"`
+	Email           string `valid:"email"`
+	Password        string `valid:"password"`
+	PasswordConfirm string `valid:"password_confirm"`
+}
+
 // DoRegister 处理注册逻辑
 func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request) {
-	//
+	// 1. 初始化数据
+	_user := user.User{
+		Name:            r.PostFormValue("name"),
+		Email:           r.PostFormValue("email"),
+		Password:        r.PostFormValue("password"),
+		PasswordConfirm: r.PostFormValue("password_confirm"),
+	}
+
+	// 2. 表单规则
+	errs := requests.ValidateRegistrationForm(_user)
+
+	if len(errs) > 0 {
+		// 3. 表单不通过 —— 重新显示表单
+		view.RenderSimple(w, view.D{
+			"Errors": errs,
+			"User":   _user,
+		}, "auth.register")
+	} else {
+		// 4. 验证成功，创建数据
+		_user.Create()
+
+		if _user.ID > 0 {
+			http.Redirect(w, r, "/", http.StatusFound)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "注册失败，请联系管理员")
+		}
+	}
+
+	// 3. 表单不通过 —— 重新显示表单
 }
---
+++ app/models/user/user.go
@@ -0,0 +1,17 @@
+package user
+
+import (
+	"goblog/app/models"
+)
+
+// User 用户模型
+type User struct {
+	models.BaseModel
+
+	Name     string `gorm:"type:varchar(255);not null;unique" valid:"name"`
+	Email    string `gorm:"type:varchar(255);unique;" valid:"email"`
+	Password string `gorm:"type:varchar(255)" valid:"password"`
+
+	// gorm:"-" —— 设置 GORM 在读写时略过此字段，仅用于表单验证
+	PasswordConfirm string `gorm:"-" valid:"password_confirm"`
+}
